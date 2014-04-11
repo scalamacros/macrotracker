e184e6ea623a6f4f72a40294a80372d1797cc4b4
Martin Duhem <martin@c-webhosting.org>
2014-04-11 16:22:43 +0200

    Fix symbols importation

--- plugin/src/main/scala/scala/tools/nsc/palladium/Attachments.scala
+++ plugin/src/main/scala/scala/tools/nsc/palladium/Attachments.scala
@@ -1,12 +1,16 @@
 package scala.tools.nsc.palladium
+import scala.tools.nsc.Global
 
-class OurAttachment(val global: scala.tools.nsc.Global) {
+class Attachments[G <: Global with Singleton](val global: G) {
 
-  def attachment = new ExpansionSummaryAttachment
+  var touchedSymbols: List[global.Symbol] = Nil
+  def attachment = ExpansionSummaryAttachment(touchedSymbols)
 
-  class ExpansionSummaryAttachment extends global.ImportableAttachment {
-    def importAttachment(importer: global.Importer) = this
-    var touchedSymbols: List[global.Symbol] = Nil
+  case class ExpansionSummaryAttachment(val touchedSymbols: List[global.Symbol]) extends global.ImportableAttachment {
+    def importAttachment(importer: global.Importer) = {
+      touchedSymbols map (sym => importer.importSymbol(sym.asInstanceOf[importer.from.Symbol]))
+      this
+    }
   }
 
 }
--- plugin/src/main/scala/scala/tools/nsc/palladium/Plugin.scala
+++ plugin/src/main/scala/scala/tools/nsc/palladium/Plugin.scala
@@ -96,9 +96,9 @@ class Plugin(val global: Global) extends NscPlugin { self =>
         override def onSuccess(expanded: Tree): Tree = {
           val result = super.onSuccess(expanded)
           loadContext(expandee).foreach(c1 => {
-            val our = new OurAttachment(global)
-            val summary = our.attachment
-            summary.touchedSymbols = c1.touchedSymbols.toList.asInstanceOf[List[our.global.Symbol]]
+            val attachments = new Attachments[global.type](global)
+            attachments.touchedSymbols = c1.touchedSymbols.toList.asInstanceOf[List[attachments.global.Symbol]]
+            val summary = attachments.attachment
             expandee.updateAttachment(summary)
             result.updateAttachment(summary)
           })
