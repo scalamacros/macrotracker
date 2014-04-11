b5190df96fc28b85d2bce7e47a5b565654b0b1a8
Martin Duhem <martin@c-webhosting.org>
2014-04-11 18:36:31 +0200

    Fix SymbolsExtractor

--- tests/src/test/scala/testutils/SymbolsExtractor.scala
+++ tests/src/test/scala/testutils/SymbolsExtractor.scala
@@ -1,9 +1,6 @@
 package testutils
 
 import testcompiler.TestCompiler
-import scala.reflect.internal._
-import scala.reflect.runtime.universe._
-import scala.tools.nsc.palladium.ExpansionSummaryAttachment
 
 import scala.language.reflectiveCalls
 
@@ -11,7 +8,7 @@ object SymbolsExtractor {
 
 	def apply(compiler: TestCompiler) = compiler.trees.map { t =>
     t.asInstanceOf[scala.reflect.internal.Trees$Tree].attachments.all.collect {
-      case esa: scala.tools.nsc.palladium.ExpansionSummaryAttachment => esa.touchedSymbols
+      case esa: { def touchedSymbols: List[_] } => esa.touchedSymbols
     }
   }.toList.flatten
 
