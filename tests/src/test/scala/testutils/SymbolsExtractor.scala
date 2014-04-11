0029a7a943dcfe285853946d693cc196929f5c7c
Martin Duhem <martin@c-webhosting.org>
2014-04-11 17:36:28 +0200

    Improve extraction of touched symbols

--- tests/src/test/scala/testutils/SymbolsExtractor.scala
+++ tests/src/test/scala/testutils/SymbolsExtractor.scala
@@ -3,18 +3,16 @@ package testutils
 import testcompiler.TestCompiler
 import scala.reflect.internal._
 import scala.reflect.runtime.universe._
-import scala.tools.nsc.palladium._
+import scala.tools.nsc.palladium.ExpansionSummaryAttachment
 
 import scala.language.reflectiveCalls
 
 object SymbolsExtractor {
 
-	def apply(compiler: TestCompiler) =
-		compiler.trees.map {
-			case t: {def attachments: scala.reflect.macros.Attachments } =>
-				t.attachments.all.collect {
-					case esa: { def touchedSymbols: List[_] } =>
-						esa.touchedSymbols
-				}.toList.flatten
-		}
+	def apply(compiler: TestCompiler) = compiler.trees.map { t =>
+    t.asInstanceOf[scala.reflect.internal.Trees$Tree].attachments.all.collect {
+      case esa: scala.tools.nsc.palladium.ExpansionSummaryAttachment => esa.touchedSymbols
+    }
+  }.toList.flatten
+
 }
