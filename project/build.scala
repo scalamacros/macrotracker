ae62a8ead62a1169d0b3de2037569dedb01c7002
Eugene Burmako <xeno.by@gmail.com>
2014-04-18 11:42:26 +0200

    upgrades to scala 2.11.0 and sbt 0.13.2

--- project/build.scala
+++ project/build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object build extends Build {
   lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.11.0-RC3",
+    scalaVersion := "2.11.0",
     crossVersion := CrossVersion.full,
     version := "0.1.0-SNAPSHOT",
     organization := "org.scalareflect",
@@ -169,7 +169,7 @@ object build extends Build {
   ) settings (
     libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
     libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _),
-    libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.2" % "test",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.3" % "test",
     libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.3" % "test",
     compile in Test := {
       sys.props("sbt.class.directory") = (classDirectory in Test).value.getAbsolutePath
